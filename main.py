d6c5d1529851da4f03c8befd84d15ce3230afddb
Harry-prd3 <harrison.baxter@ucas-edu.net>
2024-09-24 17:48:54 +0000

    renamed and each started
    Co-authored-by: non-existentxd <non-existentxd@users.noreply.github.com>
    Co-authored-by: suri-mejias <suri-mejias@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,10 @@
+#HOLAAAA :3
+input("Would you like to do a selection algorithm, Combsort Algorithjm, cycle sort algorithm, or bubble, sort lgorithm?: ")
+def combsort():
+    pass
+
+def selection_algorithm():
+    own_or_generated = input("Would you like a randomly generated list or would you like to input your own list?" )
+
+def cycle():
+    pass
